4af4554600c2cf854cbf1e90cbd32bde7898b4e1
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-29 22:51:42 -0400

    get bank rates base on selection

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,27 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IndexaApiService } from '../services/indexa-api.service';
 import * as moment from 'moment';
+import { LocalDataService } from '../services/local-data.service';
+import { BankRates } from '../interfaces/bank.interface';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor( private indexaApi: IndexaApiService ) {}
+  rates: BankRates[] = [];
+  bankCodes: any[];
 
-  getBankRates() {
+  constructor( private indexaApi: IndexaApiService,
+               private localData: LocalDataService ) {}
+
+  ngOnInit() {
+    this.bankCodes = this.localData.getBankCodes();
+  }
+
+  getBankRates( event ) {
     const today = moment();
     const sevenDaysAgo = moment().subtract(1, 'week');
 
-    this.indexaApi.getRates('bpd').subscribe(
+    this.indexaApi.getRates(event.detail.value.code).subscribe(
       resp => {
         const rates = resp.data.filter( rate =>
           new Date(rate.date) >= sevenDaysAgo.toDate()
         ).filter( rate =>
           new Date(rate.date) <= today.toDate());
-        console.log(rates);
+
+        this.rates = rates;
       }
     );
   }
