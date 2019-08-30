284e67048316ab9c27583db8435d23b9910e160a
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-29 22:11:25 -0400

    add method to return bank names and codes

--- src/app/services/local-data.service.ts
+++ src/app/services/local-data.service.ts
@@ -7,6 +7,19 @@ export class LocalDataService {
 
   constructor() { }
 
+  getBankCodes() {
+    return [
+      { name: 'Banco Popular Dominicano',	code: 'bpd'},
+      { name: 'Banco de Reservas',	code: 'bnr'},
+      { name: 'Banco del Progreso',	code: 'bpr'},
+      { name: 'Banco Santa Cruz',	code: 'bsc'},
+      { name: 'Banco BDI',	code: 'bdi'},
+      { name: 'Banco Promerica',	code: 'bpm'},
+      { name: 'Banco Vimenca',	code: 'bvm'},
+      { name: 'Banco Central de RD',	code: 'bcd'}
+    ];
+  }
+
   getRateNames( type: string ) {
     return [
       `dollar${type}rate`,
