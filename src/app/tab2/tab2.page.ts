03ff0ceb8e80a4c364eb326cd8aef76bee63e09a
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-30 18:01:11 -0400

    add datetime picker and tab2 properties to handle call to the api

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { IndexaApiService } from '../services/indexa-api.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  customDate: IonDatetime;
+  minDate: string;
+  maxDate: string;
+
+  constructor( private indexaApi: IndexaApiService ) {}
+
+  ngOnInit() {
+    this.minDate = moment().subtract(1, 'year').format('Y-MM-DD');
+    this.maxDate = moment().format('Y-MM-DD');
+  }
+
+  dateChanged( event ) {
+    
+  }
 
 }
