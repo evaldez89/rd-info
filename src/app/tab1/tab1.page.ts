ccf50200d1b4d5f31669475b6808a290b8734ea4
Emmanuel Valdez <evaldez89@gmail.com>
2019-08-29 23:24:47 -0400

    handle bank code change and on init event

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IndexaApiService } from '../services/indexa-api.service';
 import * as moment from 'moment';
 import { LocalDataService } from '../services/local-data.service';
 import { BankRates } from '../interfaces/bank.interface';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,22 +11,43 @@ import { BankRates } from '../interfaces/bank.interface';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  
+  @ViewChild(IonSegment) bankCodesSegment: IonSegment;
 
   rates: BankRates[] = [];
   bankCodes: any[];
+  slidesOpt = {
+    slidesPerView: 2.3,
+    freeMode: true,
+    spaceBetween: -5
+  };
+
 
   constructor( private indexaApi: IndexaApiService,
                private localData: LocalDataService ) {}
 
   ngOnInit() {
     this.bankCodes = this.localData.getBankCodes();
+    this.bankCodesSegment.value = this.bankCodes[0];
+    this.loadBankChange();
   }
 
-  getBankRates( event ) {
+  changeBank(event) {
+    this.bankCodesSegment.value = event.detail.value;
+    this.loadBankChange();
+  }
+
+  loadBankChange() {
+    this.rates = [];
+    this.loadBankRates();
+  }
+
+  loadBankRates() {
     const today = moment();
     const sevenDaysAgo = moment().subtract(1, 'week');
+    const code = this.bankCodesSegment.value['code'];
 
-    this.indexaApi.getRates(event.detail.value.code).subscribe(
+    this.indexaApi.getRates(code).subscribe(
       resp => {
         const rates = resp.data.filter( rate =>
           new Date(rate.date) >= sevenDaysAgo.toDate()
