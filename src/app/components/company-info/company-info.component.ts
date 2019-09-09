27f845f9a362e2433b3cf9eb41e264cb9fc8dbb6
Emmanuel Valdez <evaldez89@gmail.com>
2019-09-08 21:34:20 -0400

    add component to show company info

---
+++ src/app/components/company-info/company-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-info',
+  templateUrl: './company-info.component.html',
+  styleUrls: ['./company-info.component.scss'],
+})
+export class CompanyInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
